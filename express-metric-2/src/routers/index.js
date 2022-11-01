34fa4e5f9fcf791b8caa0e9c02814800120afd37
Lương Thiện Phuoc <phuoc.t.luong@taptap.com.vn>
2022-11-01 13:24:36 +0700

    done

--- express-metric-2/src/routers/index.js
+++ express-metric-2/src/routers/index.js
@@ -1,7 +1,11 @@
 const userRoute = require("./user")
-
+const client = require("prom-client")
 function route(app) {
   app.use("/", userRoute)
+  app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    return res.send(await client.register.metrics());
+  });
   app.use("/haisss", (req, res) => {
     res.send("ABC")
   })
